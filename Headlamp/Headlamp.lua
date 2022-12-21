752f7d6d0cdf8eb81e417a1b2d856c8486d8f3d8
Jonathan Ross <jonathan@jonathanjdr.com>
2022-12-21 02:02:24 -0500

    Update Headlamp.lua

--- Headlamp/Headlamp.lua
+++ Headlamp/Headlamp.lua
@@ -1,51 +1,37 @@
---[[
-Headlamp
-
-Creates a light on player's head to provide a light based on
-	where they're looking at.
---]]
-
--- Variables to change
+--// Attributes
 local angle = script:GetAttribute("Angle")
 local range = script:GetAttribute("Range")
 local brightness = script:GetAttribute("Brightness")
 local smoothing = script:GetAttribute("Smoothing")
 
 
--- We grab the parent of this script, which should be
--- the player's character since this script client side
+--// Player Variables
 local player = game.Players.LocalPlayer
 local char = player.Character
 local head = char:WaitForChild("Head")
 local mouse = player:GetMouse()
 
+
+--// Functions
 function createLight()
+	-- Part to anchor spotlight to
 	local headlamp = Instance.new("Part", head)
 	headlamp.Name = "headlamp"
 	
-	-- Create light and set attributes
+	headlamp.Anchored = true
+	headlamp.CanCollide = false	
+	headlamp.Transparency = 1
+	
+	headlamp.CFrame = CFrame.lookAt(head.CFrame.Position, mouse.Hit.Position)
+	
+	-- Light
 	local light = Instance.new("SpotLight", headlamp)
 	light.Name = "light"
 	light.Angle = angle
 	light.Range = range
 	light.Shadows = true
 	light.Brightness = brightness
-	
-	headlamp.Anchored = true
-	headlamp.CanCollide = false	
-	headlamp.Transparency = 1
-	
-	-- COMMENT OUT IF NOT DEBUGGING!!!
-	angle = script:GetAttribute("Angle")
-	range = script:GetAttribute("Range")
-	brightness = script:GetAttribute("Brightness")
-	smoothing = script:GetAttribute("Smoothing")
-	-- COMMENT OUT IF NOT DEBUGGING!!!
-	
-	headlamp.CFrame = CFrame.lookAt(head.CFrame.Position, mouse.Hit.Position)
 end
-
-createLight()
 
 function inputHandler(obj, _)
 	-- Handle input for light toggling
@@ -60,10 +46,8 @@ function inputHandler(obj, _)
 	end
 end
 
-local counter = 0
-
 function orientLight(step)
-	-- This will orient the part to be facing where the mouse is.
+	-- Orient light every frame to be where the mouse is
 	local headlamp = head:FindFirstChild("headlamp")
 	if headlamp then
 		-- We only want the orientation, so we remove the position
@@ -74,6 +58,7 @@ function orientLight(step)
 	end
 end
 
--- Grab Player's input
+
+--// Hooks
 game:GetService("UserInputService").InputBegan:Connect(inputHandler)
 game:GetService("RunService").Heartbeat:Connect(orientLight)
